4822d5d37e5236911acd6744bbdb1cf3e1536ba9
Tanzee <tanzeemulhaq033@gmail.com>
2026-09-24 14:10:30 +0530

    fix: switch bcrypt to bcryptjs in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 const pool = require('../config/db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 
 const logAction = async (userId, action) => {
